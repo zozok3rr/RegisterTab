2562ada746a866d7fd32f05cc5f11c975b55c958
unknown <zozok3rr@gmail.com>
2021-10-29 16:54:06 +0300

    Replaced basic functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('passwords2');
+const password2 = document.getElementById('password2');
 
 function showError(input, message) {
     const formControl = input.parentElement
@@ -22,11 +22,23 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
-
+// Check required fields 
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if(input.value.trim() == '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 // Even Listener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    
+    checkRequired([username, email, password, password2]);
     // if(username.value === '') {
     //     showError(username, 'Username is Required');
     // } else {
